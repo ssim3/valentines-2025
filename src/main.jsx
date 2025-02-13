e7f7bc518c00144e51c9dee70a9f66178b838fee
ssim3 <93330584+ssim3@users.noreply.github.com>
2025-02-13 20:49:05 +0800

    Build

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
